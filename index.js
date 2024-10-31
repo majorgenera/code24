039de5b2db9443905787a4223b8474bbee4521b6
majorgenera <majorman599@gmail.com>
2024-11-01 00:26:06 +0300

    change style + index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mainContainer = document.querySelector(".main")
 const thnkuContainer = document.querySelector(".thnku")
 const submitButton = document.getElementById("submit")
@@ -14,21 +13,4 @@ rates.forEach((rate) => {
     rate.addEventListener("click", () => {
         rating.innerHTML = rate.innerHTML
     })
-=======
-const mainContainer = document.querySelector(".main")
-const thnkuContainer = document.querySelector(".thnku")
-const submitButton = document.getElementById("submit")
-const rating = document.getElementById("rating")
-const rates = document.querySelectorAll(".btn")
-
-submitButton.addEventListener("click", () => {
-    thnkuContainer.classList.remove("hidden")
-    mainContainer.style.display = "none"
-})
-
-rates.forEach((rate) => {
-    rate.addEventListener("click", () => {
-        rating.innerHTML = rate.innerHTML
-    })
->>>>>>> dabd444bddef59517da599048bbd4efa6364474d
 })
